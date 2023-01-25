b7a260cf95dab7ce31f6648d84677515e0d8e5d4
Aliabdo6 <99968044+Aliabdo6@users.noreply.github.com>
2023-01-25 02:49:27 +0200

    update

--- website/app.js
+++ website/app.js
@@ -1,64 +1,43 @@
-/* Global Variables */
+const apiKey = '37754d4b53a0571f4274d02b3784b2aa&units=imperial';
+const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?zip=';
+const postUrl = 'http://localhost:8080/addWeatherData';
+const allDataUrl = 'http://localhost:8080/all';
 
-let baseURL = 'https://api.openweathermap.org/data/2.5/weather?zip=';
-let key = '37754d4b53a0571f4274d02b3784b2aa';
-
-// Create a new date instance dynamically with JS
-
-const newDate = new Date().toLocaleString('en-US', { month: 'long', day: 'numeric', year: 'numeric' });
-
-document.getElementById('generate').addEventListener('click', () => {
+document.getElementById('generate').addEventListener('click', async () => {
     const postCode = document.getElementById('zip').value;
     const feelings = document.getElementById('feelings').value;
-    console.log(newDate);
-
-    getTemperature(baseURL, postCode, key)
-        .then(weatherData => {
-            return postData('http://localhost:8080/addWeatherData', { temperature: weatherData.main.temp, date: newDate, user_response: feelings });
-        })
-        .then(_postResponse => {
-            updateUI();
-        })
-        .catch(error => {
-            console.log(error);
+    const date = new Date().toString().substr(0, 15);
+    // const date = new Date().toISOString().split("T")[0];
+    try {
+        const weatherData = await fetchWeather(apiUrl, postCode, apiKey);
+        postWeatherData(postUrl, { temperature: weatherData.main.temp, date: date, user_response: feelings }).then(() => {
+            console.log('done');
         });
+
+        updateWeatherUI(allDataUrl);
+    } catch (error) {
+        console.log(error);
+    }
 });
 
-const getTemperature = async (baseURL, code, key) => {
-    try {
-        const url = `  ${baseURL}${code},us&APPID=${key}`;
-        const response = await fetch(url);
-        const data = await response.json();
-        console.log(data);
-        return data;
-    } catch (error) {
-        throw new Error(error);
-    }
+const fetchWeather = async (baseUrl, zip, key) => {
+    const url = `${baseUrl}${zip},us&APPID=${key}`;
+    const response = await fetch(url);
+    return await response.json();
 }
 
-const postData = async (url, data) => {
-    try {
-        const res = await fetch(url, {
-            method: 'POST',
-            credentials: 'same-origin',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(data)
-        });
-        return newDate;
-    } catch (error) {
-        throw new Error(error);
-    }
+const postWeatherData = async (url, data) => {
+    await fetch(url, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data)
+    });
 }
 
-
-const updateUI = async () => {
-    try {
-        const request = await fetch('http://localhost:8080/all');
-        const allData = await request.json();
-        document.querySelector('#date').textContent = allData.date;
-        document.querySelector('#temp').textContent = allData.temperature;
-        document.querySelector('#content').textContent = allData.user_response;
-    } catch (error) {
-        console.error('Error:', error);
-    }
+const updateWeatherUI = async (url) => {
+    const request = await fetch(url);
+    const allData = await request.json();
+    document.querySelector('#date').textContent = allData.date;
+    document.querySelector('#temp').textContent = allData.temperature;
+    document.querySelector('#content').textContent = allData.user_response;
 }
